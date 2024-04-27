e811efc5c4e8ef7efcb094974551b67f9f09d24f
Caio Mizerkowski <caiomizerkowski@gmail.com>
2024-04-27 18:04:27 -0300

    Add selection of the episode

--- gradio/app.py
+++ gradio/app.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from pathlib import Path
 
 from natsort import natsorted
@@ -5,24 +6,28 @@ from natsort import natsorted
 import gradio as gr
 
 
+root = Path()
+
+
 class ProcessData:
     def __init__(self) -> None:
-        root = Path("a__espera_rpguaxa_153")
+        pass
 
+    def init(self, root: Path):
         self.fragments = natsorted(root.glob("fragments/*.mp3"))
         self.texts_revision = root / "revision.txt"
 
-        with open(root / "union.txt", "r") as f:
+        with open(root / "union.csv", "r") as f:
             self.texts = [t.strip() for t in f.readlines()]
 
-        try:
+        self.idx = 0
+        if self.texts_revision.exists() and self.texts_revision.stat().st_size > 0:
             with open(self.texts_revision, "r") as f:
                 last_line = f.readlines()[-1]
                 first_item = last_line.split(";")[0]
                 self.idx = int(first_item) + 2
-        except Exception as e:
-            print(e)
-            self.idx = 0
+        else:
+            self.texts_revision.touch()
 
         print(f"idx:{self.idx}")
 
@@ -55,11 +60,26 @@ class ProcessData:
         return text, audio
 
 
+def selecao(episodio: Path):
+    global root, process_data
+    root = Path(episodio)
+    process_data.init(root)
+
+
 if __name__ == "__main__":
     process_data = ProcessData()
+
+    root1 = Path("transcricoes/guaxaverso")
+    root2 = Path("transcricoes/rpguaxa")
+    chain_root = chain(root1.iterdir(), root2.iterdir())
+    episode_list = list(chain_root)
+    dropdown = gr.Dropdown(episode_list, label="Escolher episodio")
+    choice = gr.Interface(selecao, dropdown, None)
+
     inp = gr.Textbox()
     audio = gr.Audio(autoplay=True)
-    demo = gr.Interface(
+    episode = gr.Interface(
         process_data.loop, [inp, audio], [inp, audio], submit_btn="Next"
     )
-    demo.launch()
+
+    gr.TabbedInterface([choice, episode], ["Seleção", "Episodio"]).launch()
